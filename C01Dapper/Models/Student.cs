40d61ac5aba1952c9136bf7de534d06cb6076e37
Michal Stehlík <michal.stehlik@pslib.cz>
2023-11-21 15:34:34 +0100

    Add project files.

---
+++ C01Dapper/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01Dapper.Models
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public int Grade { get; set; } = 0;
+    }
+}
---
+++ C01Dapper/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dapper;
+using C01Dapper.Models;
+using System.Data.SqlClient;
+
+string cs = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dapSchool;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+
+using (var connection = new SqlConnection(cs))
+{
+    connection.Open();
+    var ver = connection.ExecuteScalar<string>("SELECT @@VERSION");
+    Console.WriteLine(ver);
+    List<Student> students = connection.Query<Student>("SELECT * FROM Students WHERE Firstname LIKE 'J%' ORDER BY Lastname").ToList();
+    foreach(Student st in students)
+    {
+        Console.WriteLine(st.Firstname + " " + st.Lastname);
+    }
+    List<string> studentNames = connection.Query<string>("SELECT Lastname FROM Students").ToList();
+    foreach (var st in studentNames)
+    {
+        Console.WriteLine(st);
+    }
+    string? newFirstname = Console.ReadLine();
+    string? newLastname = Console.ReadLine();
+    int newGrade = Random.Shared.Next(4);
+    var res = connection.Execute("INSERT INTO Students (Firstname, Lastname, Grade) VALUES ('" 
+        + newFirstname 
+        + "','"
+        + newLastname
+        + "',"
+        + newGrade
+        + ")");
+    Console.WriteLine(res);
+    var res2 = connection.Execute("UPDATE Students SET Grade = 1");
+    Console.WriteLine(res2);
+    var res3 = connection.Execute("DELETE Students WHERE Firstname = 'Jiří'");
+    Console.WriteLine(res3);
+}
