4836d79b931f7ceffc91d15015d3d295ade44e3e
Michal Stehlík <michal.stehlik@pslib.cz>
2024-02-06 15:47:02 +0100

    multi

---
+++ C11MultiPlatform/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace C11MultiPlatform
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ C11MultiPlatform/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace C11MultiPlatform
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
