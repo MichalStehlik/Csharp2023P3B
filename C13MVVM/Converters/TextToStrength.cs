d6c8679b6d617aec5b238518c60a88191c9ff8ff
Michal Stehlík <michal.stehlik@pslib.cz>
2024-02-20 15:21:46 +0100

    MVVM3

---
+++ C13MVVM/Converters/TextToStrength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C13MVVM.Converters
+{
+    internal class TextToStrength : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                if ((int)value > 10) { return "Silné"; }
+                else return "Slabé";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
