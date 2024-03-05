2904a1a98674452095b17bd87030603c0402f8f2
Michal Stehlík <michal.stehlik@pslib.cz>
2024-03-05 15:19:34 +0100

    picture commands

--- C14Command/ViewModels/MainViewModel.cs
+++ C14Command/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace C14Command.ViewModels
 {
@@ -12,7 +13,43 @@ namespace C14Command.ViewModels
     {
         private int _position = 0;
 
+        public MainViewModel()
+        {
+            NextCommand = new Command(() =>
+            {
+                Position = Position + 1;
+            }, () => { return Position < 3; });
+            PreviousCommand = new Command(() =>
+            {
+                Position = Position - 1;
+            }, () => { return Position > 0; });
+            ShiftCommand = new Command<string>((x) =>
+            {
+                Position = Convert.ToInt16(x);
+            });
+        }
 
+        public ICommand NextCommand { get; set; }
+        public ICommand PreviousCommand { get; set; }
+        public Command<string> ShiftCommand { get; set; }
+    
+        public int Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                RaisePropertyChanged();
+                (NextCommand as Command).ChangeCanExecute();
+                (PreviousCommand as Command).ChangeCanExecute();
+            }
+        }
+
+        public ICommand AlertCommand =>
+            new Command(() =>
+            {
+                App.Current.MainPage.DisplayAlert("Pozor", "Něco se děje", "OK");
+            }, () => (true));
 
         #region Binding
         public event PropertyChangedEventHandler PropertyChanged;
